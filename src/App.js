918e0338c5017e0f726adcc00f33228bbc8a4cd2
Service Unit <Marion Balba>
2021-03-09 16:22:48 +0800

    Header, data binding, props, event listeners, state hooks, prop drilling, conditional styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 // import React from 'react';
+import { useState } from 'react'
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+    const [tasks, setTasks] = useState([
+      {
+          id: 1,
+          text: 'Task 1',
+          day: 'March 9th at 1:00pm',
+          reminder: true
+      },
+      {
+          id: 2,
+          text: 'Task 2',
+          day: 'March 10th at 1:00pm',
+          reminder: false,
+      },
+      {
+          id: 3,
+          text: 'Task 3',
+          day: 'March 11th at 1:00pm',
+          reminder: true
+      },
+  ])
+
+  const deleteTask = (id) => {
+    // console.log('delete', id)
+
+    setTasks(tasks.filter((task) => task.id != id))
+  } 
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask} />):'No tasks to show'}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import Button from './Button';
 
 const Header = () => {
+
+    const onClick = (e) => {
+        console.log(e)
+    }
+
     return (
-        <header>
+        <header className="header">
             <h1>Tasker</h1>
+            <Button color="green" text="Add" onClick={onClick} />
         </header>
     )
 }
